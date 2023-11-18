a9f5102d2898785b2c11bc4b7fea9ebd31f32a1d
Gabrielle Asante <asante.gabrielle@gmail.com>
2023-11-18 18:15:22 +0000

    Add landing page

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,5 +1,27 @@
 import "./LandingPage.scss";
+import vibeScribeLogo from "../../assets/logo/vibe-scribe1.svg";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 
 export default function LandingPage() {
-  return <p>This is the landing page</p>;
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/profile");
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [navigate]);
+  return (
+    <section className="vibe-scribe">
+      <div className="vibe-scribe__container">
+        <img
+          src={vibeScribeLogo}
+          alt="vibe scribe logo"
+          className="vibe-scribe__logo vibe-scribe__fadeout"
+        />
+      </div>
+    </section>
+  );
 }
