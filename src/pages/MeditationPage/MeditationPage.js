99ce1e408323e6fe6b53df8fed402eeb9b7c3978
Gabrielle Asante <asante.gabrielle@gmail.com>
2023-11-27 23:20:25 +0000

    Refactor styling for meditation page

--- src/pages/MeditationPage/MeditationPage.js
+++ src/pages/MeditationPage/MeditationPage.js
@@ -1,10 +1,22 @@
 import { useState, useEffect } from "react";
 import "./MeditationPage.scss";
+import meditationIcon from "../../assets/icons/meditation-icon.svg";
 import ChipButton from "../../components/ChipButton/ChipButton";
 
 export default function MeditationPage() {
-  const [breathing, setBreathing] = useState(true);
   const [pickedOption, setPickedOption] = useState(null);
+  // const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+  const [breathing, setBreathing] = useState(true);
+
+  // const startAnimation = () => {
+  //   setBreathing(true);
+  //   setIsButtonDisabled(true);
+  // };
+
+  // const stopAnimation = () => {
+  //   setBreathing(false);
+  //   setIsButtonDisabled(false);
+  // };
 
   useEffect(() => {
     const breathTimer = setTimeout(() => {
@@ -22,11 +34,12 @@ export default function MeditationPage() {
 
   const chipSelections = [
     "Saddness",
-    "Anxious",
+    "Anxiety",
+    "Nervous",
     "Frustrated",
     "Stressed",
-    "Upset",
     "Angry",
+    "Weariness",
     "Annoyed",
     "Other",
   ];
@@ -34,36 +47,62 @@ export default function MeditationPage() {
     <>
       <div className="breathe">
         <div className="breathe__wrapper">
-          <h2 className="breathe__heading">Breathing Meditation</h2>
-        </div>
-        <div className="breathe__wrapper-description">
-          <p className="breathe__mindfulness">Mindfulness</p>
-          <p className="breathe__lessen">Breathe to lessen:</p>
-          <div className="breathe__list">
-            {chipSelections.map((text, index) => (
-              <ChipButton
-                key={index}
-                text={text}
-                selected={pickedOption === index}
-                onClick={() => handleChipClick(index)}
-              />
-            ))}
+          <div className="breathe__title-wrapper">
+            <h4 className="breathe__heading">Breathing Meditation</h4>
           </div>
-          <p className="breathe__description">
+          <div className="breathe__wrapper-description">
+            <div className="breathe__mindfulness-container">
+              <h3 className="breathe__mindfulness">Mindfulness:</h3>
+              <img
+                src={meditationIcon}
+                alt="meditaton icon"
+                className="breathe__meditation-icon"
+              />
+            </div>
+            <p className="breathe__description">
+              Breathing meditation helps you feel at ease, and calms your mind
+              allowing you to quiten down the noise, enabliing you to take
+              control of your thoughts. Try it out, right now, for as long as
+              you like!
+            </p>
+            <div className="breathe__lessen-container">
+              <p className="breathe__lessen">Breathe to lessen:</p>
+              <div className="breathe__list">
+                {chipSelections.map((text, index) => (
+                  <ChipButton
+                    key={index}
+                    text={text}
+                    selected={pickedOption === index}
+                    onClick={() => handleChipClick(index)}
+                  />
+                ))}
+              </div>
+              {/* <p className="breathe__description">
             Breathing meditation helps you feel at ease, helps you relaxe and
             allows you to quiten down the noise, and have some time to be
             mindful. Try it out, right now!
-          </p>
-        </div>
-        <div
-          className={`breathe__container ${
-            breathing ? "breathe__in" : "breathe__out"
-          }`}
-        >
-          <div className="breathe__circle-wrapper">
-            <p className="breathe__text">
-              {breathing ? "Breathe In" : " And Breathe Out"}
-            </p>
+          </p> */}
+            </div>
+            <div
+              className={`breathe__container ${
+                breathing ? "breathe__in" : "breathe__out"
+              }`}
+            >
+              <div className="breathe__circle-wrapper">
+                <p className="breathe__text">
+                  {breathing ? "Breathe In" : " And Breathe Out"}
+                </p>
+              </div>
+            </div>
+            {/* <div className="breathe__button-container">
+              <button
+                onClick={startAnimation}
+                disabled={isButtonDisabled}
+                className="breathe__button"
+              >
+                Start Animation
+              </button>
+            </div> */}
           </div>
         </div>
       </div>
