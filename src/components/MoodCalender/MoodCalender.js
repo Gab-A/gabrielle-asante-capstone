ca5bfa70bf0a6fc41cfbb8ac7ab53db4f6235d39
Gabrielle Asante <asante.gabrielle@gmail.com>
2024-01-16 12:42:28 +0000

    Refactor bottom nav bar and add tablet styling design to tracker page and mood calender component

--- src/components/MoodCalender/MoodCalender.js
+++ src/components/MoodCalender/MoodCalender.js
@@ -8,25 +8,16 @@ import { useState, useEffect } from "react";
 import dayjs from "dayjs";
 import Lottie from "lottie-react";
 import noAnimation from "../../assets/animations/no.json";
-import happyEmoji from "../../assets/icons/smile.svg";
-import sadEmoji from "../../assets/icons/sad.svg";
-import anxiousEmoji from "../../assets/icons/anxious.svg";
-import tiredEmoji from "../../assets/icons/tired.svg";
-import stressedEmoji from "../../assets/icons/stressed.svg";
-import gratefulEmoji from "../../assets/icons/grateful.svg";
-import unsureEmoji from "../../assets/icons/unsure.svg";
-import calmEmoji from "../../assets/icons/calm.svg";
-import angryEmoji from "../../assets/icons/angry.svg";
 
 export default function Calender({ cardsArray }) {
-  const [moods, setMoods] = useState([]);
+  const [selectedDateMoods, setSelectedDateMoods] = useState([]);
   const [selectedDate, setSelectedDate] = useState(null);
 
   useEffect(() => {
     const getAndSetMoods = async () => {
       try {
         const response = await getMoods();
-        setMoods(response);
+        setSelectedDateMoods(response);
         if (response.length > 0) {
           setSelectedDate(response[0].date);
         }
@@ -47,28 +38,12 @@ export default function Calender({ cardsArray }) {
     ? dayjs(selectedDate).startOf("day").format("DD-MM-YYYY")
     : "";
 
-  const selectedDateMoods = moods.filter((mood) => {
+  const filterSelectedDateMoods = selectedDateMoods.filter((mood) => {
     const formattedMoodDate = dayjs(mood.created_at)
       .startOf("day")
       .format("DD-MM-YYYY");
     return formattedMoodDate === formattedSelectedDate;
   });
-
-  // const moodEmoji = {
-  //   Grateful: gratefulEmoji,
-  //   Tired: tiredEmoji,
-  //   Happy: happyEmoji,
-  //   Sad: sadEmoji,
-  //   Angry: angryEmoji,
-  //   Calm: calmEmoji,
-  //   Unsure: unsureEmoji,
-  //   Anxious: anxiousEmoji,
-  //   Stressed: stressedEmoji,
-  // };
-
-  // const getMoodEmoji = (mood) => {
-  //   return moodEmoji[mood];
-  // };
 
   const getMoodEmoji = (mood) => {
     const card = cardsArray.find((c) => c.title === mood);
@@ -76,61 +51,63 @@ export default function Calender({ cardsArray }) {
   };
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DateCalendar
-        value={selectedDate || null}
-        onChange={handleDateChange}
-        className="tracker__calender"
-      />
-      <h4 className="tracker__text">
-        Get on track to see your different moods. It is exciting to so see how
-        much you have evolved.
-      </h4>
-      <p></p>
-
-      {selectedDateMoods.length > 0 && (
-        <div className="tracker__mood-wrapper">
-          <h4 className="tracker__different-moods">
-            Your different moods on{" "}
-            <span className="tracker__mood-formatted-date">
-              {formattedSelectedDate}:{" "}
-            </span>
-          </h4>
-          <div className="tracker__mood-container">
-            {selectedDateMoods.map((mood) => (
-              <article key={mood.id} className="tracker__card">
-                <div className="tracker__mood-details-wrapper">
-                  <p className="tracker__mood">{mood.mood}</p>
-                  <img
-                    src={`${getMoodEmoji(mood.mood)}`}
-                    alt={mood.mood}
-                    className="tracker__mood-icon"
-                  />
-                  <p className="tracker__mood-time">
-                    {mood.created_at.slice(11, 16)}
-                  </p>
-                </div>
-              </article>
-            ))}
-          </div>
+    <div className="tracker__mood-flex">
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DateCalendar
+          value={selectedDate || null}
+          onChange={handleDateChange}
+          className="tracker__calender"
+        />
+        <h4 className="tracker__text">
+          Get on track to see your different moods. It is exciting to so see how
+          much you have evolved.
+        </h4>
+        <div className="tracker__mood-flex1">
+          {filterSelectedDateMoods.length > 0 && (
+            <div className="tracker__mood-wrapper">
+              <h4 className="tracker__different-moods">
+                Your different moods on{" "}
+                <span className="tracker__mood-formatted-date">
+                  {formattedSelectedDate}:
+                </span>
+              </h4>
+              <div className="tracker__mood-container">
+                {filterSelectedDateMoods.map((mood) => (
+                  <article key={mood.id} className="tracker__card">
+                    <div className="tracker__mood-details-wrapper">
+                      <p className="tracker__mood">{mood.mood}</p>
+                      <img
+                        src={`${getMoodEmoji(mood.mood)}`}
+                        alt={mood.mood}
+                        className="tracker__mood-icon"
+                      />
+                      <p className="tracker__mood-time">
+                        {mood.created_at.slice(11, 16)}
+                      </p>
+                    </div>
+                  </article>
+                ))}
+              </div>
+            </div>
+          )}
+          {filterSelectedDateMoods.length === 0 && (
+            <div className="tracker__no-mood-wrapper">
+              <p className="tracker__no-mood">
+                No moods were logged on this date
+                <span className="tracker__mood-formatted-date">
+                  {""}
+                  {formattedSelectedDate}
+                </span>
+              </p>
+              <Lottie
+                animationData={noAnimation}
+                loop={false}
+                className="tracker__no-animation"
+              />
+            </div>
+          )}
         </div>
-      )}
-      {selectedDateMoods.length === 0 && (
-        <div className="tracker__no-mood-wrapper">
-          <p className="tracker__no-mood">
-            No moods were logged on this day{" "}
-            <span className="tracker__mood-formatted-date">
-              {formattedSelectedDate}
-            </span>
-            .
-          </p>
-          <Lottie
-            animationData={noAnimation}
-            loop={false}
-            className="tracker__no-animation"
-          />
-        </div>
-      )}
-    </LocalizationProvider>
+      </LocalizationProvider>
+    </div>
   );
 }
